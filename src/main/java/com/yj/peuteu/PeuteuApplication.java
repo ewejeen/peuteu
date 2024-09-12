c2bd562c97d2c5f6134f53bc4a16539492fa1142
ewejeen <yoojinkim24@gmail.com>
2024-09-12 13:21:49 +0900

    Initialized project

---
+++ src/main/java/com/yj/peuteu/PeuteuApplication.java
@@ -0,0 +1,13 @@
+package com.yj.peuteu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeuteuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeuteuApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yj/peuteu/PeuteuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yj.peuteu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeuteuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
